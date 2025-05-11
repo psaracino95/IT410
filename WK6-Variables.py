42de9d274417d0d5a1e3c117920928ac8cda47cf
psaracino95 <pjsaracino@gmail.com>
2025-05-11 16:35:26 -0400

    Add files via upload
    Added Week 6 homework

---
+++ WK6-Variables.py
@@ -0,0 +1,27 @@
+first_name=("peter")
+last_name=("SARACINO")
+
+print("Hello,",first_name.upper(),last_name.lower(),"\n", "\n")
+#There is like 3 ways to do quotes, I chose the backslash
+print("\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible\" - Francis of Assisi")
+
+#6
+# Store two decimal numbers
+num1 = 1200.6
+num2 = 40.4
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"{num1} plus {num2} equals {addition}")
+print(f"{num1} minus {num2} equals {subtraction}")
+print(f"{num1} times {num2} equals {multiplication}")
+print(f"{num1} divided by {num2} equals {division}")
+
+#Month
+month = "May"
+day_of_month = 10
+#Don't forget the two tabs
+print(f"\n\t\tToday is the day, it is {day_of_month} of the month {month}")
